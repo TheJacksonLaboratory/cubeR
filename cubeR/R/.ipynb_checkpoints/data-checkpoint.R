7349cd723e186317b21ca309d290d2be4bf3d96d
Hongping Liang <hongpingliang@gmail.com>
2020-10-08 22:16:03 +0000

    init

---
+++ cubeR/R/.ipynb_checkpoints/data-checkpoint.R
@@ -0,0 +1,40 @@
+#' @title Cube Metadata Assay 
+#'
+#' @name Cube Metadata Assay
+#'
+#' @description Metadata information for assay
+#'
+#' @format A assay metadata with 18 rows and 5 variables:
+#' \describe{
+#'   \item{element_id}{assay id}
+#'   \item{element_label}{name of assay}
+#'   \item{instance_id}{assay instance id}
+#'   \item{date_created}{date created}
+#'   \item{property value}{assay property value}
+#' }
+"metadata_assay"
+
+#' @source <https://www.github.com/TheJacksonLaboratory/cube_r>
+"snp_data"
+
+#' @title SNP Grid Data
+#'
+#' @name SNP Grid Data
+#'
+#' @description A sample data set for SNP Grid Data.
+#'
+#' @format A SNP data frame with 1000 rows and 10 variables:
+#' \describe{
+#'   \item{chr}{chromosome number, eg: 6}
+#'   \item{start_position}{start position}
+#'   \item{end_position}{end position}
+#'   \item{rs}{rs}
+#'   \item{strain_id}{strain id}
+#'   \item{strain_name}{strain name}
+#'   \item{genotyp}{genotype}
+#'   \item{imputed_flag}{imputed flag}
+#'   \item{percent_impluted}{percent impluted}
+#'   \item{percent_missing}{percent missing}
+#' }
+#' @source <https://www.github.com/TheJacksonLaboratory/cube_r>
+"snp_data"
