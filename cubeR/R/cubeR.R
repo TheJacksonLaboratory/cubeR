01a2c0f3c791db50b138e957ddaa2c7302c9b209
Hongping Liang <hongpingliang@gmail.com>
2020-10-09 15:15:44 +0000

    init

---
+++ cubeR/R/cubeR.R
@@ -0,0 +1,12 @@
+#' A CubeR Function
+#'
+#' This is basic function to retrieve cube data
+#' @param dataset_id dataset to access.
+#' @keywords cubeR
+#' @export
+#' @examples
+#' cubeR("jaxda00234")
+cubeR <- function(dataset_id="jaxda00234"){
+    print( paste("dataset: ", dataset_id) )
+    return (metadata_assay)
+}
---
+++ cubeR/R/hello.R
@@ -0,0 +1,11 @@
+#' A Hello World Function
+#'
+#' This is echo function to test installation.
+#' @param message
+#' @keywords Hello
+#' @export
+#' @examples
+#' hello()
+hello <- function(message="World"){
+    print( paste("Hello: ", message) )
+}
