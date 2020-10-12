6c6915732483f15ffd17e6abe09db86403fabfce
Hongping Liang <hongpingliang@gmail.com>
2020-10-12 16:12:20 +0000

    init

--- cubeR/R/.ipynb_checkpoints/cubeR-checkpoint.R
+++ cubeR/R/.ipynb_checkpoints/cubeR-checkpoint.R
@@ -1,13 +1,22 @@
 #' A CubeR Function
 #'
 #' This is basic function to retrieve cube data
-#' @param dataset_id dataset to access.
+#' @param dataset dataset to access.
+#' @param accession_ids accession_ids to access.
 #' @keywords cubeR
 #' @export
 #' @examples
-#' cubeR("jaxda00234")
-cubeR <- function(dataset_id="jaxda00234"){
-    print( paste("dataset: ", dataset_id) )
-    return (assay)
-    
+#' cubeR("Assay", "JAXAS00005")
+cubeR <- function(dataset="Assay", accession_ids="JAXAS00005"){
+    print( paste("dataset: ", dataset) )
+    print( paste("accession_ids: ", accession_ids) )
+   
+    data = switch(   
+        dataset,   
+        "Assay" = assay,   
+        "Disease" = disease,   
+        "Drug_Treatment" = drug_treatment,   
+        "Mouse_Strain"= mouse_strain 
+    ) 
+    return (data)
 }
