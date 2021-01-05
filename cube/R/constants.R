b3c787edc41e62e25dac06ca804e3bb49c359733
Hongping Liang <hongping.liang@jax.org>
2021-01-05 13:44:43 -0500

    add

--- cube/R/constants.R
+++ cube/R/constants.R
@@ -11,12 +11,12 @@ HTTP_METHOD_POST = 2
 
 HTTP_METHOD_LABELS = list("GET", "POST")
 
-API_END_POINT_metadata_definition_element = "metadata_definition/element"
-API_END_POINT_metadata_repository_element_instance = "metadata_repository/element_instance"
+API_END_POINT_metadata_definition_element = "metadata_definition/element/"
+API_END_POINT_metadata_repository_element_instance = "metadata_repository/element_instance/"
 # leave the trial slash on, post will not work if no trial slash
 API_END_POINT_metadata_repository_element_instance_filter = "metadata_repository/element_instance/filter/"
-API_END_POINT_cube_about_disclaimer = "cube_about/disclaimer"
-API_END_POINT_data_store_files = "/data_store/files"
+API_END_POINT_cube_about_disclaimer = "cube_about/disclaimer/"
+API_END_POINT_data_store_files = "/data_store/files/"
 API_END_POINT_metadata_repository_collection = "metadata_repository/collection/"
 
 USER_ACCESS_TOKEN_FILE = "~/.cube_user_access_token.RData"
